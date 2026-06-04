3174371b58f77ac91d9b2ce11edd2c25fbd58f2c
Claudio Sandro Quispesivana Torres <u202215099@upc.edu.pe>
2026-06-03 22:25:12 -0500

    Update app title to 'TamizAI-APP' in MaterialApp configuration

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class TamizAIApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TamizAI',
+      title: 'TamizAI-APP',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
